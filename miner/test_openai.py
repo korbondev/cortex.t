14e22acb6b34ff4bc51f1cbf9ea523da7eafc2cf
i-beck <99846875+i-beck@users.noreply.github.com>
2024-08-08 12:33:34 -0400

    added image gen testing to testopenai script

--- miner/test_openai.py
+++ miner/test_openai.py
@@ -14,9 +14,14 @@ from alt_key_handler import get_endpoint_overrides
 
 ENDPOINT_OVERRIDE_MAP = get_endpoint_overrides()
 
-api_key = ENDPOINT_OVERRIDE_MAP["ENVIRONMENT_KEY"][ENDPOINT_OVERRIDE_MAP["ServiceEndpoint"].get("OpenRouter", {}).get("ENVIRONMENT_KEY", "")]
+api_key = ENDPOINT_OVERRIDE_MAP["ENVIRONMENT_KEY"][ENDPOINT_OVERRIDE_MAP["ServiceEndpoint"].get("yourownai", {}).get("ENVIRONMENT_KEY", "")]
 base_url = ENDPOINT_OVERRIDE_MAP["ServiceEndpoint"].get("OpenRouter", {}).get("api", "")
 client = AsyncOpenAI(api_key=api_key, base_url=base_url, timeout=60)
+
+
+api_key = ENDPOINT_OVERRIDE_MAP["ENVIRONMENT_KEY"][ENDPOINT_OVERRIDE_MAP["ServiceEndpoint"].get("yourownai", {}).get("ENVIRONMENT_KEY", "")]
+base_url = ENDPOINT_OVERRIDE_MAP["ServiceEndpoint"].get("AIMLAPI", {}).get("api", "")
+image_client = AsyncOpenAI(api_key=api_key, base_url=base_url, timeout=60)
 
 
 async def send_openai_request(prompt, engine="gpt-4-1106-preview"):
@@ -54,14 +59,53 @@ async def send_openai_request(prompt, engine="gpt-4-1106-preview"):
         return "Error calling model"
 
 
-async def main():
+async def send_openai_image_request(
+    prompt="a jeep ride through aruba on a majestical dirt sunny road in the forest",
+    model="unknown",
+):
+    try:
+        response = await image_client.images.generate(
+            model=model,
+            prompt=prompt,
+            size="1792x1024",
+            quality="standard",  # or hd (double the cost)
+            style="vivid",  # or naturual
+            n=1,
+        )
+
+        image_url = response.data[0].url
+        image_revised_prompt = response.data[0].revised_prompt
+        image_created = response.created
+        return f"created at {image_created}\n\nrevised_prompt = {image_revised_prompt}\n\nurl = {image_url}"
+
+    except Exception as e:
+        print(f"Got exception when calling openai {e}")
+        traceback.print_exc()
+        return "Error calling model"
+
+
+async def llm_test():
     prompts = ["count to 10", "tell me a joke"]
-    # tasks = [send_openai_request(prompt) for prompt in prompts]
-    tasks = [send_openai_request("")]
+    tasks = [send_openai_request(prompt) for prompt in prompts]
+    # tasks = [send_openai_request("")]
 
     responses = await asyncio.gather(*tasks)
     for response in responses:
         print(response)
 
 
-asyncio.run(main())
+async def dalle_test():
+    # prompts = ["count to 10", "tell me a joke"]
+    # tasks = [send_openai_request(prompt) for prompt in prompts]
+    model = "dall-e-3"
+    tasks = [send_openai_image_request(model=ENDPOINT_OVERRIDE_MAP["LlmModelMap"].get(model, {}).get("ModelName", ""))]
+
+    responses = await asyncio.gather(*tasks)
+    for response in responses:
+        print(response)
+
+
+# asyncio.run(llm_test())
+
+
+asyncio.run(dalle_test())
